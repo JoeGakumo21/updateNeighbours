dee030bd1df15d6157d6c253d285415ff980315f
Joe Gakumo <joegakumo1@gmail.com>
2021-10-30 00:10:40 +0300

    add register functionality

--- main/views.py
+++ main/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .forms import CreateUserForm
+
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 # creating my logic here
 def registerPage(request):
-    context={}
+    form=CreateUserForm
+
+    if request.method=='POST':
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+    context={'form':form}
     return  render (request,'accounts/register.html', context)
 
 
