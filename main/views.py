2ea471c6d337ff7faddb185c24265772ef103223
Joe Gakumo <joegakumo1@gmail.com>
2021-10-29 22:45:37 +0300

    create two functions for reister and login

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
 # Create your views here.
+# creating my logic here
+def registerPage(request):
+    context={}
+    return  render (request,'account/register.html', context)
+
+
+# login logic here
+def loginPage(request):
+    context={}
+    return  render (request,'account/login.html', context)
