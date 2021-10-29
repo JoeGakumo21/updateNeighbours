0053f4930cd9833f1536c932ecad869b627d98aa
Joe Gakumo <joegakumo1@gmail.com>
2021-10-30 00:47:34 +0300

    add login and redirect to home page

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from .import views
 urlpatterns = [
    path('register/', views.registerPage, name="register"),
    path('login/',views.loginPage, name="login"),
+
+   path('',views.home, name="home")
 ]
--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpRequest
 from .forms import CreateUserForm
 from  django.contrib import messages
-
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 # creating my logic here
@@ -14,7 +14,7 @@ def registerPage(request):
         if form.is_valid():
             form.save()
             user=form.cleaned_data.get("username")
-            messages.success(request, 'Account was created for' + user)
+            messages.info(request, 'Account was created for ' +  user)
             return redirect('login')
 
     context={'form':form}
@@ -23,5 +23,20 @@ def registerPage(request):
 
 # login logic here
 def loginPage(request):
+
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        
+
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
     context={}
     return  render (request,'accounts/login.html', context)
+
+def home(request):
+    content={}
+    return render(request, 'home.html', content)
