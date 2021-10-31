493696f8e482eed5bff83f5c36c9d0262053bcc2
Joe Gakumo <joegakumo1@gmail.com>
2021-10-31 11:22:23 +0300

    add pillow and models classes for category and for newsdetails

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.expressions import F
 
 # Create your models
+class Category(models.Model):
+    name=models.CharField(max_length=200, null=False, blank=False)
+    posted_by=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+# news details
+class NewsDetails(models.Model):    
+    image=models.ImageField(null=False, blank=False)
+    Category=models.ForeignKey(Category, on_delete=models.SET_NULL)
+    date_posted=models.CharField(max_length=30)    
+    description=models.CharField(max_length=1300, null=False, blank=False)
+    postedby=models.CharField(max_length=200)
